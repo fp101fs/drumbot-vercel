92780704b8cb2331bcc160e512903815a6096405
fp101fs <136630859+fp101fs@users.noreply.github.com>
2026-01-06 23:36:58 +0700

    Simplify script loading and fix scoping issues

--- audio/input-recorder.js
+++ audio/input-recorder.js
@@ -1,97 +1,94 @@
-document.addEventListener('DOMContentLoaded', () => {
-  /**
-   * @license
-   * Copyright 2019 Google Inc. All Rights Reserved.
-   *
-   * Licensed under the Apache License, Version 2.0 (the "License");
-   * you may not use this file except in compliance with the License.
-   * You may obtain a copy of the License at
-   *
-   *    http://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software
-   * distributed under the License is distributed on an "AS IS" BASIS,
-   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   * See the License for the specific language governing permissions and
-   * limitations under the License.
-   */
+/**
+ * @license
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-  class InputRecorder {
-    constructor() {
-      this.isRecordingInput = false;
-      this.isPlaying = false;
-      this.player = new core.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
-      this.player.callbackObject = {
-        run: (note) => {
-          this.viz.redraw(note, true);
-        },
-        stop: () => {}
-      };
-      this.reset();
-    }
-
-    reset() {
-      this.stop();
-      this.input = {notes:[], tempos: [{time: 0, qpm: 120}], totalTime: 0};
-      this.full = {notes:[], tempos: [{time: 0, qpm: 120}], totalTime: 0};
-      this.isRecordingInput = false;
-      this.offset = 0;
-      this.updateVisualizer();
-    }
-
-    setBpm(bpm) {
-      this.input.tempos[0].qpm = bpm;
-      this.full.tempos[0].qpm = bpm;
-    }
-
-    updateVisualizer() {
-      this.viz = new core.PianoRollSVGVisualizer(this.full, document.getElementById('svgInput'), {
-        noteHeight: 5,
-        pixelsPerTimeStep: 30,
-        minPitch: 21,
-        maxPitch: 108
-      });
-    }
-
-    startRecordingInput(offset) {
-      this.isRecordingInput = true;
-      this.offset = offset;
-      this.input.notes = [];
-    }
-
-    stopRecordingInput() {
-      this.isRecordingInput = false;
-    }
-
-    saveInputNote(note) {
-      const cloned = core.sequences.clone(note);
-      cloned.startTime -= this.offset;
-      cloned.endTime -= this.offset;
-      this.input.notes.push(cloned);
-    }
-
-    saveMelodyNote(note) {
-      this.full.notes.push(note);
-    }
-
-    getInput(totalTime) {
-      this.input.totalTime = totalTime;
-      return this.input;
-    }
-
-    start(callback) {
-      this.isPlaying = true;
-      this.player.start(this.full).then(callback);
-    }
-
-    stop() {
-      this.isPlaying = false;
-      this.player.stop();
-    }
-
-    addLoops(audioLoop, inputOffset) {
-      audioLoop.addLoops(this.full, inputOffset, -1);
-    }
+class InputRecorder {
+  constructor() {
+    this.isRecordingInput = false;
+    this.isPlaying = false;
+    this.player = new core.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
+    this.player.callbackObject = {
+      run: (note) => {
+        this.viz.redraw(note, true);
+      },
+      stop: () => {}
+    };
+    this.reset();
   }
-  window.InputRecorder = InputRecorder;
-});
+
+  reset() {
+    this.stop();
+    this.input = {notes:[], tempos: [{time: 0, qpm: 120}], totalTime: 0};
+    this.full = {notes:[], tempos: [{time: 0, qpm: 120}], totalTime: 0};
+    this.isRecordingInput = false;
+    this.offset = 0;
+    this.updateVisualizer();
+  }
+
+  setBpm(bpm) {
+    this.input.tempos[0].qpm = bpm;
+    this.full.tempos[0].qpm = bpm;
+  }
+
+  updateVisualizer() {
+    this.viz = new core.PianoRollSVGVisualizer(this.full, document.getElementById('svgInput'), {
+      noteHeight: 5,
+      pixelsPerTimeStep: 30,
+      minPitch: 21,
+      maxPitch: 108
+    });
+  }
+
+  startRecordingInput(offset) {
+    this.isRecordingInput = true;
+    this.offset = offset;
+    this.input.notes = [];
+  }
+
+  stopRecordingInput() {
+    this.isRecordingInput = false;
+  }
+
+  saveInputNote(note) {
+    const cloned = core.sequences.clone(note);
+    cloned.startTime -= this.offset;
+    cloned.endTime -= this.offset;
+    this.input.notes.push(cloned);
+  }
+
+  saveMelodyNote(note) {
+    this.full.notes.push(note);
+  }
+
+  getInput(totalTime) {
+    this.input.totalTime = totalTime;
+    return this.input;
+  }
+
+  start(callback) {
+    this.isPlaying = true;
+    this.player.start(this.full).then(callback);
+  }
+
+  stop() {
+    this.isPlaying = false;
+    this.player.stop();
+  }
+
+  addLoops(audioLoop, inputOffset) {
+    audioLoop.addLoops(this.full, inputOffset, -1);
+  }
+}
--- audio/metronome.js
+++ audio/metronome.js
@@ -1,73 +1,69 @@
-document.addEventListener('DOMContentLoaded', () => {
-  /**
-   * @license
-   * Copyright 2019 Google Inc. All Rights Reserved.
-   *
-   * Licensed under the Apache License, Version 2.0 (the "License");
-   * you may not use this file except in compliance with the License.
-   * You may obtain a copy of the License at
-   *
-   *    http://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software
-   * distributed under the License is distributed on an "AS IS" BASIS,
-   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   * See the License for the specific language governing permissions and
-   * limitations under the License.
-   */
-  const Tone = window.Tone;
+/**
+ * @license
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const Tone = window.Tone;
 
-  class Metronome {
-    constructor(numQuarters) {
-      this.numQuarters = numQuarters;
-      this.isTicking = false;
-      this.muted = false;
-      this.reset();
-    }
-
-    reset() {
-      if (this.part) {
-        this.part.stop();
-        this.part.dispose();
-      }
-      this.synth = new Tone.MembraneSynth().toMaster();
-      this.synth.volume.value = -10;
-      this.startedAt = undefined;
-    }
-
-    start(bpm, callbacks) {
-      this.reset();
-      Tone.Transport.bpm.value = bpm;
-      const part = new Tone.Part((time, value) => {
-        this.synth.triggerAttackRelease(value.note, '16n', time);
-        if (this.muted) return;
-        if (value.tick % this.numQuarters === 0) {
-          callbacks.barMark(time);
-        }
-        if (value.tick % 1 === 0) {
-          callbacks.quarterMark(time, value.tick % this.numQuarters);
-        }
-        callbacks.clickMark(time, value.tick);
-
-      }, [{tick:0, note: 'c2'}, {tick:1, note: 'c1'}, {tick:2, note: 'c1'}, {tick:3, note: 'c1'}]);
-      part.loop = true;
-      part.loopEnd = '1m';
-
-      this.part = part;
-      this.isTicking = true;
-      this.startedAt = Tone.immediate();
-      this.part.start(this.startedAt);
-    }
-
-    stop() {
-      this.isTicking = false;
-      this.part.stop(Tone.immediate());
-    }
-
-    timeish() {
-      return Tone.immediate() - this.startedAt;
-    }
+class Metronome {
+  constructor(numQuarters) {
+    this.numQuarters = numQuarters;
+    this.isTicking = false;
+    this.muted = false;
+    this.reset();
   }
 
-  window.Metronome = Metronome;
-});
+  reset() {
+    if (this.part) {
+      this.part.stop();
+      this.part.dispose();
+    }
+    this.synth = new Tone.MembraneSynth().toMaster();
+    this.synth.volume.value = -10;
+    this.startedAt = undefined;
+  }
+
+  start(bpm, callbacks) {
+    this.reset();
+    Tone.Transport.bpm.value = bpm;
+    const part = new Tone.Part((time, value) => {
+      this.synth.triggerAttackRelease(value.note, '16n', time);
+      if (this.muted) return;
+      if (value.tick % this.numQuarters === 0) {
+        callbacks.barMark(time);
+      }
+      if (value.tick % 1 === 0) {
+        callbacks.quarterMark(time, value.tick % this.numQuarters);
+      }
+      callbacks.clickMark(time, value.tick);
+
+    }, [{tick:0, note: 'c2'}, {tick:1, note: 'c1'}, {tick:2, note: 'c1'}, {tick:3, note: 'c1'}]);
+    part.loop = true;
+    part.loopEnd = '1m';
+
+    this.part = part;
+    this.isTicking = true;
+    this.startedAt = Tone.immediate();
+    this.part.start(this.startedAt);
+  }
+
+  stop() {
+    this.isTicking = false;
+    this.part.stop(Tone.immediate());
+  }
+
+  timeish() {
+    return Tone.immediate() - this.startedAt;
+  }
+}
